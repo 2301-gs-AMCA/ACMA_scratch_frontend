0923a1c2694acd25430530e74f4aca8226ef5c40
Matthew Rowley <mattrowley10@gmail.com>
2023-06-24 08:35:58 -0600

    work on profile, needs media query

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,3 +1,27 @@
 export default function Profile() {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <h1 className="userHeader">Welcome, User!</h1>
+      <div className="userInfo">
+        <ul className="userInfo">
+          <li>username</li>
+          <li>recently purchased items</li>
+          <li>security info</li>
+          <li>settings</li>
+        </ul>
+      </div>
+      <div className="itemsULike">
+        <h2>Items You May Like</h2>
+        <ul className="likedItems">
+          <li>List of items based on your search history</li>
+        </ul>
+      </div>
+      <div className="orderHistory">
+        <ul className="history">
+          <h2>Order History</h2>
+          <li className="historyItems">Your Recently Ordered Items</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
